d0f5e435e5f10b8dfcf3b3a04b91671a979b1001
origami8 <origami8@gmail.com>
2021-12-20 20:03:49 +0100

    changed structure to one class per day

---
+++ src/com/example/Day1.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day1 {
+    static List<Integer> slidingWindows(List<Integer> numbers){
+        ArrayList<Integer> slidingWindow = new ArrayList<Integer>();
+        for(int i = 2; i< numbers.size(); i++) {
+            int prepreviousnumber = numbers.get(i-2);
+            int previousnumber = numbers.get(i-1);
+            int currentnumber = numbers.get(i);
+            int sum = prepreviousnumber + previousnumber + currentnumber;
+            slidingWindow.add(new Integer(sum));
+        }
+//        for(int i=0; i< slidingWindow.size(); i++) {
+//            System.out.println(slidingWindow.get(i));
+//        }
+        return slidingWindow;
+    }
+
+    static void compareWithPrevious(List<Integer> numbers) {
+        int count = 0;
+        for(int i = 1; i< numbers.size(); i++) {
+            int previousnumber = numbers.get(i-1);
+            int currentnumber = numbers.get(i);
+            if (currentnumber > previousnumber){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -2,7 +2,6 @@ package com.example;
 
 import utils.TestUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -11,36 +10,10 @@ public class Main {
 
         List<Integer> numbers = TestUtils.readFileIntoListInteger("input/input.txt");
         System.out.println("Day1: First Solution:");
-        compareWithPrevious(numbers);
+        Day1.compareWithPrevious(numbers);
         System.out.println("Day1: Second Solution:");
-        List<Integer> slidingWindow = slidingWindows(numbers);
-        compareWithPrevious(slidingWindow);
+        List<Integer> slidingWindow = Day1.slidingWindows(numbers);
+        Day1.compareWithPrevious(slidingWindow);
     }
 
-    private static List<Integer> slidingWindows(List<Integer> numbers){
-        ArrayList<Integer> slidingWindow = new ArrayList<Integer>();
-        for(int i = 2; i< numbers.size(); i++) {
-            int prepreviousnumber = numbers.get(i-2);
-            int previousnumber = numbers.get(i-1);
-            int currentnumber = numbers.get(i);
-            int sum = prepreviousnumber + previousnumber + currentnumber;
-            slidingWindow.add(new Integer(sum));
-        }
-//        for(int i=0; i< slidingWindow.size(); i++) {
-//            System.out.println(slidingWindow.get(i));
-//        }
-        return slidingWindow;
-    }
-
-    private static void compareWithPrevious(List<Integer> numbers) {
-        int count = 0;
-        for(int i = 1; i< numbers.size(); i++) {
-            int previousnumber = numbers.get(i-1);
-            int currentnumber = numbers.get(i);
-            if (currentnumber > previousnumber){
-                count++;
-            }
-        }
-        System.out.println(count);
-    }
 }
